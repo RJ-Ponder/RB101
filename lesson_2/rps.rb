ba5dfc0d06784cf2930dea7b765e35f775693882
RJ Ponder <rjponder@gmail.com>
2021-03-03 03:03:15 +0000

    cleaned up rps game

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,87 +1,63 @@
-VALID_CHOICES = [:rock, :paper, :scissors, :lizard, :spock]
-VALID_CHOICES_ABBREVIATED = [:r, :p, :s, :l, :k]
+VALID_CHOICES = %w(rock paper scissors lizard spock)
+VALID_CHOICES_ABBREVIATED = %w(r p s l k)
 
 def prompt(message)
   puts("=> #{message}")
 end
 
-def convert_abbreviation(abbreviation)
-  convert_hash = {
-                  r: "rock",
-                  p: "paper",
-                  s: "scissors",
-                  l: "lizard",
-                  k: "spock"
-                  }
-  convert_hash[abbreviation]
+def convert_abbreviation(letter)
+  letter_word_pairs = {
+    'r' => 'rock',
+    'p' => 'paper',
+    's' => 'scissors',
+    'l' => 'lizard',
+    'k' => 'spock'
+  }
+  letter_word_pairs[letter]
 end
 
-def display_score(player_wins, computer_wins)
-  if player_wins
-    #increment score
-  elsif computer_wins
-    #increment score
-  else
-    # don't increment score bc of tie.
-  end
-  puts "Score: You (#{player_wins}); Computer (#{computer_wins}})"
-end
+winning_move_combinations = {
+  'rock' => ['lizard', 'scissors'],
+  'paper' => ['rock', 'spock'],
+  'scissors' => ['paper', 'lizard'],
+  'lizard' => ['spock', 'paper'],
+  'spock' => ['rock', 'scissors']
+}
 
-=begin
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
-end
-
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You win!")
-  elsif win?(computer, player)
-    prompt("Computer wins!")
-  else
-    prompt("It's a tie!")
-  end
-end
-=end
-
-possible_moves = { rock: ['lizard', 'scissors'],
-                   paper: ['rock', 'spock'],
-                   scissors: ['paper', 'lizard'],
-                   lizard: ['spock', 'paper'],
-                   spock: ['rock', 'scissors']
-                  }
+prompt("Welcome to rock, paper, scissors, lizard, spock!")
+prompt("This game is best of five.")
 
 player_score = 0
 computer_score = 0
 
 loop do
-  choice = ''
-  
-  loop do
-    prompt("Choose one: rock (r), paper (p), scissors (s), lizard (l), spock (k).")
-    
-    choice = gets.chomp.downcase.to_sym
+  player_choice = ''
 
-    if VALID_CHOICES.include?(choice)
+  loop do
+    prompt('Choose one: rock (r), paper (p), scissors (s),'\
+      ' lizard (l), spock (k).')
+
+    player_choice = gets.chomp.downcase
+
+    if VALID_CHOICES.include?(player_choice)
       break
-    elsif VALID_CHOICES_ABBREVIATED.include?(choice)
-      choice = convert_abbreviation(choice).to_sym
+    elsif VALID_CHOICES_ABBREVIATED.include?(player_choice)
+      player_choice = convert_abbreviation(player_choice)
       break
     else
       prompt("Please make a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample.to_sym
-  
-  puts("You chose #{choice}; Computer chose #{computer_choice}.")
-  
-  if possible_moves[choice].include?(computer_choice.to_s)
+  computer_choice = VALID_CHOICES.sample
+
+  puts("You chose #{player_choice}; Computer chose #{computer_choice}.")
+
+  if winning_move_combinations[player_choice].include?(computer_choice)
     prompt("You win!")
     player_score += 1
-  elsif possible_moves[computer_choice].include?(choice.to_s)
-    prompt("You lose!")
+  elsif winning_move_combinations[computer_choice].include?(player_choice)
+    prompt("Computer wins!")
     computer_score += 1
   else
     prompt("It's a tie!")
@@ -89,7 +65,6 @@ loop do
 
   prompt("Score: You #{player_score}; Computer #{computer_score}")
 
-  # display_results(choice, computer_choice)
   if player_score == 5
     prompt("You are the champion!")
     break
@@ -97,9 +72,10 @@ loop do
     prompt("Computer is the champion!")
     break
   else
-    prompt("Continue best of five?")
+    prompt("Continue playing to best of five? Enter to continue.")
     answer = gets.chomp
-    break unless answer.downcase().start_with?('y')
+    break unless answer == ''
+    system('clear')
   end
 end
 
